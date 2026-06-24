8f0ce8bc2978fd0cd38d0eb2888c67beb3d8c63c
annisaalpitri24 <annisa62144@sma.belajar.id>
2026-06-24 16:33:32 +0700

    Update index.blade.php

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -1,24 +1,10 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Data User</title>
-</head>
-<body>
-
-<h2>Data User</h2>
-
-<a href="/users/create">+ Tambah User</a>
-
-@if(session('success'))
-    <p style="color:green">{{ session('success') }}</p>
-@endif
-
 <table border="1" cellpadding="10">
     <tr>
         <th>No</th>
         <th>Nama</th>
         <th>Email</th>
         <th>Role</th>
+        <th>Status</th>
         <th>Aksi</th>
     </tr>
 
@@ -28,20 +14,49 @@
         <td>{{ $user->name }}</td>
         <td>{{ $user->email }}</td>
         <td>{{ $user->role }}</td>
-        <td>
-            <a href="/users/{{ $user->id }}/edit">Edit</a>
 
-            <form action="/users/{{ $user->id }}" method="POST" style="display:inline;">
+        <td>
+            @if($user->status == 'pending')
+            <span style="color:orange;">
+                Pending
+            </span>
+            @else
+            <span style="color:green;">
+                Aktif
+            </span>
+            @endif
+        </td>
+
+        <td>
+
+            @if($user->status == 'pending')
+           <form action="{{ route('users.approve', $user->id) }}" method="POST">
+                @csrf
+
+                <button type="submit" class="btn btn-success btn-action">
+                    <i class="fa-solid fa-check"></i>
+                    Setujui
+                </button>
+            </form>
+            @endif
+
+            <a href="/users/{{ $user->id }}/edit">
+                Edit
+            </a>
+
+            <form action="/users/{{ $user->id }}"
+                method="POST"
+                style="display:inline;">
                 @csrf
                 @method('DELETE')
-                <button type="submit" onclick="return confirm('Hapus user ini?')">
+
+                <button type="submit"
+                    onclick="return confirm('Hapus user ini?')">
                     Hapus
                 </button>
             </form>
+
         </td>
     </tr>
     @endforeach
 </table>
-
-</body>
-</html>
